225f02aaab483f8924c682edafb4132f81ba4b90
miyeonglee <miyeong1596@gmail.com>
2021-05-27 15:07:36 +0900

    index

--- index/js/index.js
+++ index/js/index.js
@@ -7,3 +7,15 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+
+  $(".mMenu").click(function () {
+    $(".menu").toggleClass('on');
+  });
+
+
+  $(".box").click(function () {
+    $(this).toggleClass('on');
+    $(".box").not(this).removeClass("on");
+  });
