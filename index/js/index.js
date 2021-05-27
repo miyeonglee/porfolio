ae87222ae5326c8a48ce288f269c25b9ca881460
miyeonglee <miyeong1596@gmail.com>
2021-05-27 16:42:47 +0900

    링크으으으ㅡ~!

--- index/js/index.js
+++ index/js/index.js
@@ -19,3 +19,29 @@ var swiper = new Swiper(".mySwiper", {
     $(this).toggleClass('on');
     $(".box").not(this).removeClass("on");
   });
+
+
+
+// 타이핑 효과
+  function autoType(elementClass, typingSpeed){
+    var thhis = $(elementClass);
+    thhis = thhis.find(".typing");
+    
+    var text = thhis.text().trim().split('');
+    var amntOfChars = text.length;
+    var newString = "";
+    setTimeout(function(){
+        for(var i = 0; i < amntOfChars; i++){
+            (function(i,char){
+                setTimeout(function() {        
+                    newString += char;
+                    thhis.text(newString);
+                },i*typingSpeed);
+            })(i+1,text[i]);
+        }
+    },1500);
+}
+
+$(document).ready(function(){
+    autoType(".main", 150);
+});
