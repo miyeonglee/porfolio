5b69634dbdf0c29faa458eecb2657a73872c43f5
miyeonglee <miyeong1596@gmail.com>
2021-05-30 22:51:42 +0900

    Revert "올려"
    This reverts commit dabc6d3116b6d35fe99651f43a5a883ea2dad1c2.

--- lecordonbleu/js/lecordonbleu.js
+++ lecordonbleu/js/lecordonbleu.js
@@ -1,4 +1,5 @@
 
+$(document).ready(function () {
 
 
   // 공지 팝업창
@@ -19,7 +20,7 @@
   $('#closeCheck').click(function () {
     $('.notice').css('display', 'none');
   });
-  
+
   $("#draggable").draggable();
 
 
@@ -29,13 +30,7 @@
     $(".campus").slideToggle();
   });
   
-  $(".mcampus").hide();
-  $(".mcampusbutton").click(function () {
-    $(this).next().slideToggle();
-    $(".mcampus").not(this).next().hide();
-  });
 
-  
   $(".closemap").click(function () {
     $(".campusGate").hide();
     $(".campus").hide();
@@ -45,38 +40,101 @@
     $(".campus").fadeToggle();
   });
 
-  //nav
-  $(".mMenu").click(function () {
-    $("nav").addClass("on");
-    $(".top").hide();
-    $(".quickmenu").hide();
-
-  });
-  $(".mX").click(function () {
-    $("nav").removeClass("on");
-    $(".top").show();
-    $(".quickmenu").show();
+  // visual section
+  var swiper1 = new Swiper('.swiper-container.visual', {
+    spaceBetween: 30,
+    effect: 'fade',
+    autoplay: true,
   });
 
 
-  $(".menu > li > div").click(function () {
-    $(this).next().toggleClass("on");
-    $(".menu > li > div").not(this).next().removeClass("on");
+  $(".se1 > .youtube").click(function () {
+    $(".se1 > iframe").toggleClass("on");
+  });
+
+  $(".se5 .online").click(function () {
+    $(".se5 .open, .se5 .formopen").toggleClass("on");
+    $(".personal").find(".pb").removeClass("on");
+  });
+
+  $(".se5 .pb").click(function () {
+    $(this).toggleClass("on");
+    $(".se5 .pb").not(this).removeClass("on");
     return false;
   });
- 
 
-  //퀵메뉴
-  var iw = window.innerWidth;
-  if (iw < 1081) {
-    $(".quickmenu .quick").hide();
-    $(".quickmenu > div").click(function () {
-      $(".quickmenu .quick").slideToggle();
-    });
-  }
-  
+  $(".mMenu").click(function () {
+    $("header, nav, .logo, .msearch, .mX, .mMenu, .langbutton, .form").addClass("on");
+    $(".top").hide();
+  });
+  $(".mX").click(function () {
+    $("header, nav, .logo, .msearch, .mX, .mMenu, .langbutton, .form").removeClass("on");
+    $(".top").show();
+  });
 
 
+
+  $(".ffoo > li > div").click(function () {
+    $(this).next().toggleClass("on");
+    $(".ffoo > li > div").not(this).next().removeClass("on");
+    $(".fo").find("span").removeClass("on");
+    return false;
+  });
+
+  $(".fo > div").click(function () {
+    $(this).siblings().toggleClass("on");
+    $(".ffoo").find(".sub").removeClass("on");
+    return false;
+  });
+
+  $(".menu > li > a").click(function () {
+    $(this).next().toggleClass("on");
+    $(".menu > li > a").not(this).next().removeClass("on");
+  });
+
+  // $(".pprg > div").click(function () {
+  //     $(this).children(".firstp").toggleClass("on");
+  //     $(".pprg > div").not(this).children(".firstp").removeClass("on");
+  // });
+
+  $(".pprg > div").hover(function () {
+    $(this).toggleClass("on");
+    $(".pprg > div").not(this).removeClass("on");
+  });
+
+  $(window).resize(function () {
+    if ($(window).width() < 641) {
+      $(".pprg > div").click(function () {
+        $(this).children(".firstp").toggleClass("on");
+        $(".pprg > div").not(this).children(".firstp").removeClass("on");
+      });
+    }
+  });
+
+  // top버튼
+  $(".top").hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > $("header").height()) {
+      $(".top").fadeIn("swing");
+    } else {
+      $(".top").fadeOut("swing");
+    }
+  });
+
+
+
+
+
+
+
+  var lastWidth = $(window).width();
+  $(window).resize(function () {
+    if ($(window).width() != lastWidth) {
+      location.reload();
+      lastWidth = $(window).width();
+      return false;
+    }
+  });
 
 
 
@@ -95,82 +153,6 @@
   })
 
 
-
-//프로그램
-if (iw > 1081) {
-  $(".prgbox > div").hover(function () {
-    $(".prgbox > div").toggleClass("on");
-    $(".prgbox > div").not(this).removeClass("on");
-  });
-}else{
-    $(".prgbox > div").click(function () {
-      $(".prgbox > div").toggleClass("on");
-      $(".mremove").hide(); 
-      $(".prgbox > div").not(this).removeClass("on");
-    }
-)};
-
-if (iw > 1081) {
-  $(".pbutton").hover(function () {
-    $(".pbutton").toggleClass("on");
-  });
-}
-
-  // top버튼
-  $(".top").hide();
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > $("header").height()) {
-      $(".top").fadeIn("swing");
-    } else {
-      $(".top").fadeOut("swing");
-    }
-  });
-
-  $( '.top' ).click( function() {
-    $( 'html, body' ).animate( { scrollTop : 0 }, 1000 );
-    return false;
-  } );
-
-
-  var lastWidth = $(window).width();
-  $(window).resize(function () {
-    if ($(window).width() != lastWidth) {
-      location.reload();
-      lastWidth = $(window).width();
-      return false;
-    }
-  });
-
-
-
-
-
-
-
-if (iw > 1081) {
-  $(".img > div").hover(function () {
-    $(".img > div").toggleClass("on");
-    $(".img > div").not(this).removeClass("on");
-  });
-}else{
-    $(".img > div").click(function () {
-      $(".img > div").toggleClass("on");
-      $(".img > div").not(this).removeClass("on");
-    }
-)};
-
-
-
-
-$(".se4 .online").click(function () {
-  $(".se4 .open, .se4 .formopen").toggleClass("on");
-  $(".personal").find(".pb").removeClass("on");
-});
-
-$(".se4 .pb").click(function () {
-  $(this).toggleClass("on");
-  $(".se4 .pb").not(this).removeClass("on");
-  return false;
 });
 
 
@@ -179,93 +161,10 @@ $(".se4 .pb").click(function () {
 
 
 
-
-
-// 유효성 검사
-
-var email = RegExp(/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i);
-var pass = RegExp(/^\d{3}-\d{3,4}-\d{4}$/);
-var named = RegExp(/^[가-힣]+$/);
-
-
-$("#name").keyup(function () {
-  // 이름 공백 확인
-  if ($("#name").val() == "") {
-      $("#namelog").text("이름을 입력해주세요");
-      $("#name").focus();
-      return false;
-  }    //이름 유효성 검사
-  else if (!named.test($("#name").val())) {
-      $("#namelog").text("이름형식에 맞게 입력해주세요");
-      $("#name").focus();
-      return false;
-  }
-  else { // 합당한 경우
-    $("#namelog").text("");
-}
-});
-
-// 전화번호 유효성 검사
-$("#tel").keyup(function () {
-  // 전화번호 공백 확인
-  if ($("#tel").val() == "") {
-      $("#tellog").text("전화번호를 입력해주세요");
-      $("#tel").focus();
-      return false;
-  }
-  // 전화번호 정규식으로 테스트
-  else if (!pass.test($("#tel").val())) {
-      $("#tellog").text("형식에 맞게 입력해주세요");
-      $("#tel").focus();
-      return false;
-  } else { // 합당한 경우
-      $("#tellog").text("");
-  }
-});
-
-
-//이메일 유효성 검사
-$("#email").keyup(function () {
-  //이메일 공백 검사
-  if ($("#email").val() == "") {
-      $("#emaillog").text("이메일을 입력 해주세요");
-      $("#email").focus();
-      return false;
-  }
-  //이메일 유효성 검사
-  else if (!email.test($("#email").val())) {
-      $("#emaillog").text("이메일형식에 맞게 입력해주세요");
-      $("#email").focus();
-      return false;
-  } else { // 서로 맞는경우
-      $("#emaillog").text("");
-  }
-});
-
-
-
-
-$(".right > li > div").click(function () {
-  $(this).next().toggleClass("on");
-  $(".right > li > div").not(this).next().removeClass("on");
-  $(".lcb").find("span").removeClass("on");
-  return false;
-});
-
-$(".lcb > div").click(function () {
-  $(this).siblings().toggleClass("on");
-  $(".right").find(".sub").removeClass("on");
-  return false;
-});
 
 
 // 스와이퍼
-  // visual section
-  var swiper1 = new Swiper('.swiper-container.visual', {
-    spaceBetween: 30,
-    effect: 'fade',
-    autoplay: true,
-  });
+
 
 var swiper2 = new Swiper('.swiper-container.pswiper', {
   autoplay: true,
@@ -283,15 +182,15 @@ var swiper3 = new Swiper('.swiper-container.inews', {
     el: '.swiper-pagination',
     clickable: true,
   },
-
   breakpoints: {
-    641: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    1081: {
+    640: {
       slidesPerView: 3,
-      spaceBetween: 10,
     }
   },
 });
+
+
+
+
+
+
