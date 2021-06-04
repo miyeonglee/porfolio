aff2bb78817ea63e89185cc8cee99db68d459861
miyeonglee <miyeong1596@gmail.com>
2021-06-04 10:28:21 +0900

    올리기

---
+++ index/js/index.js
@@ -0,0 +1,51 @@
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    mousewheel: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+
+  $(".mMenu").click(function () {
+    $(".menu").toggleClass('on');
+  });
+
+
+  $(".box").click(function () {
+    $(this).toggleClass('on');
+    $(".box").not(this).removeClass("on");
+  });
+
+
+
+// 타이핑 효과
+  function autoType(elementClass, typingSpeed){
+    var thhis = $(elementClass);
+    thhis = thhis.find(".typing");
+    
+    var text = thhis.text().trim().split('');
+    var amntOfChars = text.length;
+    var newString = "";
+    setTimeout(function(){
+        for(var i = 0; i < amntOfChars; i++){
+            (function(i,char){
+                setTimeout(function() {        
+                    newString += char;
+                    thhis.text(newString);
+                },i*typingSpeed);
+            })(i+1,text[i]);
+        }
+    },1500);
+}
+
+$(document).ready(function(){
+    autoType(".main", 150);
+});
+
+$(".button").click(function () {
+  $(this).toggleClass('on');
+});
